e9abaffdf0aa6b179cc1fb3f12d22c95e22cfe22
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-11-24 20:41:12 +0000

    design the layout for slide for the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationComponent } from '../navigation/navigation.component';
+import { SwiperContainer, register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavigationComponent],
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('swiperContainer') swiperContainer!: SwiperContainer
 
+  ngAfterViewInit(): void {
+    const swiperParams = {
+      slidesPerView: 1,
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+      centerSlides: true,
+      centeredSlidesBounds: true,
+      on: {
+        init() {
+          // ...
+        },
+      },
+    };
+
+    // now we need to assign all parameters to Swiper element
+    Object.assign(this.swiperContainer, swiperParams);
+
+    // and now initialize it
+    this.swiperContainer.initialize();
+  }
 }
