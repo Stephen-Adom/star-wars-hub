a5eee4eb33b99f63945d6ba5a9d7e7551c0c1c75
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-11-25 00:09:16 +0000

    add dynamic table to lister page

--- src/app/lister-page/lister-page.component.ts
+++ src/app/lister-page/lister-page.component.ts
@@ -1,13 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lister-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableModule, ButtonModule],
   templateUrl: './lister-page.component.html',
   styleUrls: ['./lister-page.component.scss']
 })
-export class ListerPageComponent {
+export class ListerPageComponent implements OnInit, OnDestroy {
+  categoryList: any[] = [];
+  routeSubscription = new Subscription();
+  category!: string | null;
+  tableHeader = [
+    {
+      label: 'Name',
+      value: 'name'
+    },
+    {
+      label: 'Height',
+      value: 'height'
+    },
+    {
+      label: 'Mass',
+      value: 'mass'
+    },
+    {
+      label: 'Hair Color',
+      value: 'hair_color'
+    },
+    {
+      label: 'Skin Color',
+      value: 'skin_color'
+    },
+    {
+      label: 'Eye Color',
+      value: 'eye_color'
+    },
+    {
+      label: 'Gender',
+      value: 'gender'
+    }
+  ]
 
+  data: any = [
+    {
+      name: 'Luke Skywalker',
+      height: '172',
+      mass: '77',
+      hair_color: 'blond',
+      skin_color: 'fair',
+      eye_color: 'blue',
+      gender: 'male'
+    }
+  ]
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.routeSubscription = this.route.paramMap.subscribe(data => {
+      this.category = data.get('category');
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
+  }
 }
